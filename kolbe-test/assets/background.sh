d6313e089a25525cf7eccaa0524c011837aaf02c
Kolbe Kegel <kolbe@kolbekegel.com>
2021-07-13 17:37:58 -0700

    Fixed md

--- kolbe-test/assets/background.sh
+++ kolbe-test/assets/background.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 declare -a pid
-{ curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh; } &
-pid+=($!)
 { apt update -y && apt install -y mariadb-client; } &
 pid+=($!)
 wait "${pid[@]}"
